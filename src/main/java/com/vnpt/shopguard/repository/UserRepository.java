d9143d60f043135fafc72b076f246e86e98acde3
dlb <dlb.hoang0603@gmail.com>
2025-06-25 07:04:24 -0700

    fix

--- src/main/java/com/vnpt/shopguard/repository/UserRepository.java
+++ src/main/java/com/vnpt/shopguard/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.vnpt.shopguard.repository;
 
-import com.vnpt.shopguard.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vnpt.shopguard.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
